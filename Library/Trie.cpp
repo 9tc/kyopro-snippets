aed064d36553c575e474fb25c9d432c91665dc2f
9tc <38623525+9tc@users.noreply.github.com>
2022-06-13 13:43:54 +0900

    update Trie.cpp

--- Library/Trie.cpp
+++ Library/Trie.cpp
@@ -1,19 +1,21 @@
 template <char margin = 'A', int char_size = 26>
 struct Trie {
+
   struct TrieNode {
-    std::array<int, char_size> node;
+    array<int, char_size> node;
     TrieNode() { node.fill(-1); };
-    typename std::array<int, char_size>::iterator begin() { return node.begin(); }
-    typename std::array<int, char_size>::iterator end() { return node.end(); }
+    typename array<int, char_size>::iterator begin() { return node.begin(); }
+    typename array<int, char_size>::iterator end() { return node.end(); }
     int& operator[](int i) { return node[i]; }
   };
-  std::vector<TrieNode> tree;
-  std::vector<int> num;  // 部分木内の個数
+
+  vector<TrieNode> tree;
+  vector<int> num;  // 部分木内の個数
   Trie() : tree(1), num(1) {}
-  void insert(const std::string& s) {
+  void insert(const string& s) {
     ++num[0];
     int t = 0;
-    for (char c: s) {
+    for(char c: s) {
       int d = c - margin;
       if(tree[t][d] == -1) {
         tree[t][d] = tree.size();
@@ -25,14 +27,14 @@ struct Trie {
     }
   }
 
-  void erase(const std::string& s) {
+  void erase(const string& s) {
     int par = 0;
     --num[par];
-    for (char c : s) {
+    for(char c: s) {
       int d = c - margin;
       int nxt = tree[par][d];
       assert(nxt != -1);
-      if (--num[nxt] == 0) tree[par][d] = -1;
+      if(--num[nxt] == 0) tree[par][d] = -1;
       par = nxt;
     }
   }
