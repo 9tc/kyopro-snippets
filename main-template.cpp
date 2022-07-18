9b85c962ac29931ab773ec52bac7d1a9e9e71892
9tc <38623525+9tc@users.noreply.github.com>
2022-07-18 21:17:27 +0900

    update template

--- main-template.cpp
+++ main-template.cpp
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 using ll = long long;
-#define REP(i, n) for (ll i = 0; (i) < ll(n); ++ (i))
-#define FOR(i, m, n) for (ll i = (m); (i) <= ll(n); ++ (i))
-#define REPR(i, n) for (ll i = ll(n) - 1; (i) >= 0; -- (i))
-#define FOR3R(i, m, n) for (ll i = ll(n) - 1; (i) >= ll(m); -- (i))
+#define REP(i, n) for(ll i = 0; (i) < ll(n); ++ (i))
+#define FOR(i, m, n) for(ll i = (m); (i) <= ll(n); ++ (i))
+#define REPR(i, n) for(ll i = ll(n) - 1; (i) >= 0; -- (i))
+#define FORR(i, m, n) for(ll i = ll(n) - 1; (i) >= ll(m); -- (i))
 #define ALL(x) x.begin(),x.end()
 
 #define INF (int)1e9
 #define LLINF (long long)1e18
 #define MOD (int)1e9+7
+#define MOD9 (int)998244353
 #define PI 3.141592653589
 #define PB push_back
 #define F first
@@ -19,33 +20,31 @@ using ll = long long;
 #define YesNo(T) if(T){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
 #define Yes(T) {cout<<"Yes"<<endl; if(T) return 0;}
 #define No(T) {cout <<"No"<<endl; if(T) return 0;}
-
+#define YES(T) {cout<<"YES"<<endl; if(T) return 0;}
+#define NO(T) {cout <<"NO"<<endl; if(T) return 0;}
 
 #define Graph vector<vector<int> >
 #define CostGraph vector<vector<pair<int,ll> > >
 #define PII pair<int,int>
+#define PLL pair<ll,ll>
 #define VI vector<int>
 #define VL vector<ll>
 #define VVI vector<vector<int> >
 #define VVL vector<vector<ll> >
 #define VPII vector<pair<int,int> >
+#define VPLL vector<pair<ll,ll> >
 
 #define DDD fixed<<setprecision(10)
+#define PAD setfill('0')<<right<<setw(8)
 
 template <class T>
 inline bool chmin(T &a, T b) {
-	if (a > b) {
-		a = b;
-		return true;
-	}
+	if(a > b){ a = b; return true;}
 	return false;
 }
 template <class T>
 inline bool chmax(T &a, T b) {
-	if (a < b) {
-		a = b;
-		return true;
-	}
+	if(a < b){a = b; return true;}
 	return false;
 }
 template <class T>
@@ -61,12 +60,16 @@ template <class T>
 inline T input(){
   T t; std::cin >> t; return t;
 }
+template <class T>
+inline void printVec(std::vector<T> v){
+  REP(i,v.size()) std::cout << v[i] << " \n"[i == v.size()-1];
+}
 
 using namespace std;
-/*
-#include <atcoder/all>
-using namespace atcoder;
-*/
+
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint998244353;
 
 int main(){
   ios::sync_with_stdio(false);
